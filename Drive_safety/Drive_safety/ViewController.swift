26b73cdd45eb73f19218532b2ae41da6e9f8a30e
Gyeongseop Lee <deathkill200@naver.com>
2020-06-07 18:11:38 +0900

    Init Drive Safety

---
+++ Drive_safety/Drive_safety/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Drive_safety
+//
+//  Created by Game on 2020/06/07.
+//  Copyright © 2020 Game. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
