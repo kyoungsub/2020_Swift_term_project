c35c4f9916d6439c16514251e86a1603a08eef9e
KDY9687 <jungsang3488@gmail.com>
2020-06-18 19:49:10 +0900

    MapPin debug

--- Drive_safety/Drive_safety/MapViewController.swift
+++ Drive_safety/Drive_safety/MapViewController.swift
@@ -1,35 +1,36 @@
 //
 //  MapViewController.swift
-//  Drive_safety
+//  HospitalMap
 //
-//  Created by Game on 2020/06/18.
-//  Copyright © 2020 Game. All rights reserved.
+//  Created by kpugame on 2020/05/19.
+//  Copyright © 2020 DONGYEOP KIM. All rights reserved.
 //
 
 import UIKit
 import MapKit
-
 class MapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
-    
     var posts = NSMutableArray()
+    
     let regionRadius: CLLocationDistance = 5000
     
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        
         mapView.setRegion(coordinateRegion, animated: true)
     }
     
-    var spots : [Spot] = []
+    var spots: [Spot] = []
     
     func loadInitialData() {
         for post in posts {
             let spot_nm = (post as AnyObject).value(forKey: "spot_nm") as! NSString as String
+            
             let XPos = (post as AnyObject).value(forKey: "lo_crd") as! NSString as String
             let YPos = (post as AnyObject).value(forKey: "la_crd") as! NSString as String
-            let lat = (XPos as NSString).doubleValue
-            let lon = (YPos as NSString).doubleValue
+            let lat = (YPos as NSString).doubleValue
+            let lon = (XPos as NSString).doubleValue
             let spot = Spot(title: spot_nm, coordinate: CLLocationCoordinate2D(latitude: lat, longitude: lon))
             spots.append(spot)
         }
@@ -41,36 +42,37 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         location.mapItem().openInMaps(launchOptions: launchOptions)
     }
     
+    func mapView(_  mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    guard let annotation = annotation as? Spot else { return nil }
     
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        guard let annotation = annotation as? Spot else { return nil }
-        
-        let identifier = "marker"
-        var view: MKMarkerAnnotationView
-        
-        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView {
-            dequeuedView.annotation = annotation
-            view = dequeuedView
-        } else {
-            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-            view.canShowCallout = true
-            view.calloutOffset = CGPoint(x: -5, y: 5)
-            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        }
-        
-        return view
+    let identifier = "marker"
+    var view: MKMarkerAnnotationView
+    
+    if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView {
+        dequeuedView.annotation = annotation
+        view = dequeuedView
+    }
+    else {
+        view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        view.canShowCallout = true
+        view.calloutOffset = CGPoint(x: -5, y: 5)
+        view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+    }
+    return view
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         let initialLocation = CLLocation(latitude: 37.5384514, longitude: 127.0709764)
+        
         centerMapOnLocation(location: initialLocation)
+        
         mapView.delegate = self
         loadInitialData()
         mapView.addAnnotations(spots)
     }
+    
 
     /*
     // MARK: - Navigation
